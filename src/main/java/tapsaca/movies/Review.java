629974ff0232114c4f0e77c906d1701a7a0ff38e
tapsaca <98756983+tapsaca@users.noreply.github.com>
2023-09-15 22:17:33 +0300

    Add classes Movie and Review

---
+++ src/main/java/tapsaca/movies/Review.java
@@ -0,0 +1,18 @@
+package tapsaca.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@Data
+@Document(collection = "reviews")
+@NoArgsConstructor
+public class Review {
+  @Id
+  private ObjectId id;
+  private String body;
+}
