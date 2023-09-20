0fbd0b1a66cccd4b278d767d1dbb220a0ff24c65
tapsaca <98756983+tapsaca@users.noreply.github.com>
2023-09-20 23:23:51 +0300

    Implement fetching of all movies from the database

---
+++ src/main/java/tapsaca/movies/MovieService.java
@@ -0,0 +1,15 @@
+package tapsaca.movies;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieService {
+  @Autowired
+  private MovieRepository movieRepository;
+
+  public List<Movie> allMovies() {
+    return movieRepository.findAll();
+  }
+}
